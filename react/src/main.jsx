babe11a2cef7a94163a2ed62389ad5700a75aa3a
Isaac Pozos <isaac.pozos@ogilvy.com>
2023-04-10 11:01:10 -0600

    Inicio Reducer

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -12,9 +12,10 @@ import ReactDOM from 'react-dom/client'
 import { CallbackHook } from './06-memos/CallbackHook';
 
 import './index.css'
+import './08-useReducer/intro-reducer';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CallbackHook />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <CallbackHook />
+//   </React.StrictMode>,
+// )
